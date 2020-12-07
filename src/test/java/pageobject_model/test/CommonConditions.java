cd599539da820fba759ac56e237037c595516c3b
VladislavRb <vlad.raba@mail.ru>
2020-12-07 10:16:35 +0300

    implement class with common conditions for all tests

---
+++ src/test/java/pageobject_model/test/CommonConditions.java
@@ -0,0 +1,22 @@
+package pageobject_model.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+public class CommonConditions {
+    protected WebDriver driver;
+    private static final String RESOURCES_PATH = "\\src\\main\\resources";
+
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetup() {
+        System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void browserQuit() {
+        driver.quit();
+    }
+}
