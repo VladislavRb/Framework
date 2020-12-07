032a60ab386a79d92cfa98a82dac4e8f94ef3105
VladislavRb <vlad.raba@mail.ru>
2020-12-08 01:10:16 +0300

    refactor common conditions class

--- src/test/java/test/CommonConditions.java
+++ src/test/java/test/CommonConditions.java
@@ -1,20 +1,27 @@
 package test;
 
 import driver.DriverSingleton;
+import util.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({TestListener.class})
 public class CommonConditions {
+
     protected WebDriver driver;
 
-    @BeforeMethod(alwaysRun = true)
-    public void browserSetup() {
+
+    @BeforeMethod()
+    public void browserSetup()
+    {
         driver = DriverSingleton.getDriver();
     }
 
     @AfterMethod(alwaysRun = true)
-    public void browserQuit() {
+    public void browserQuit()
+    {
         DriverSingleton.closeDriver();
     }
 }
